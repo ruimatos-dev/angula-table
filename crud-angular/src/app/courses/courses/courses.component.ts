5c9f615f63640f090f98517cea82aee746eaeb4c
Rui Matos <rui.microk@gmail.com>
2021-11-26 21:07:19 -0300

    Tabela Dados Funcionando

---
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+
+  courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'front-end' }
+  ];
+  displayedColumns = ['name','category'];
+
+  constructor() {
+    // this.courses = [];
+  }
+
+  ngOnInit(): void {
+  }
+
+}
